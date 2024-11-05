09251c10ac071b638f93ecd79bb761c7614ff09a
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2024-11-05 12:51:26 -0500

    t-SNE Calculation and Plots
    Calculate the t-SNE dimensionality reduction and plot it in a scatter subplot

--- VariablePCAtSNE.py
+++ VariablePCAtSNE.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_s
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats import zscore
+from sklearn.manifold import TSNE
 
 # Read in the csv file that contains all trial data
 dataFile = np.loadtxt('Data/data1N.txt')
@@ -24,24 +25,39 @@ print(amplitudes)
 print(magnitudes)
 print(signs)
 
+allVars = np.concatenate((amplitudes, magnitudes, signs), 1)
+print(allVars)
+
 #print(dataFile)
 
-# Initialize the KNN Classifier
-knn = KNeighborsClassifier(n_neighbors=3)
+# t-SNE dimensionality reduction
+tsneAmp = TSNE().fit_transform(amplitudes)
+tsneMag = TSNE().fit_transform(magnitudes)
+tsneSig = TSNE().fit_transform(signs)
 
-# Perform cross-validated predictions
-y_pred = cross_val_predict(knn, X, y, cv=4)
+print(tsneAmp.shape)
+print(tsneMag.shape)
+print(tsneSig.shape)
 
-# Calculate the confusion matrix
-cmlabels = [1,2,3,4,5,6,7,8,9]
-conf_matrix = confusion_matrix(y, y_pred)
-disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix)
-disp.plot(cmap='Blues')
-plt.title("Confusion Matrix (KNN Classification)")
-plt.xticks(ticks=np.arange(len(cmlabels)) , labels=cmlabels )
-plt.yticks(ticks=np.arange(len(cmlabels)) , labels=cmlabels)
+# t-SNE plotting code
+# Credit: https://www.futurelearn.com/info/courses/machine-learning-for-image-data/0/steps/362489
+plt.set_cmap('tab10')
+
+fig, ax = plt.subplots(1, 3, figsize = (20,5))
+scatter1 = ax[0].scatter(tsneAmp[:,0], tsneAmp[:,1], c = y)
+legend1 = ax[0].legend(*scatter1.legend_elements())
+legend1.set_draggable(True)
+ax[0].add_artist(legend1)
+
+scatter2 = ax[1].scatter(tsneMag[:,0], tsneMag[:,1], c = y)
+legend2 = ax[1].legend(*scatter2.legend_elements())
+legend2.set_draggable(True)
+ax[1].add_artist(legend2)
+
+scatter3 = ax[2].scatter(tsneSig[:,0], tsneSig[:,1], c = y)
+legend3 = ax[2].legend(*scatter3.legend_elements())
+legend3.set_draggable(True)
+ax[2].add_artist(legend3)
+
 plt.show()
-
-# Calculate and display the accuracy score
-accuracy = accuracy_score(y, y_pred)
-print(f"Cross-validated Accuracy: {accuracy:.2f}")
+plt.close()
