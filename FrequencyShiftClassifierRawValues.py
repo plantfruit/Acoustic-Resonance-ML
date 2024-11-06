95a3ed135eb21fc8de36744caa922088be78d3a7
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2024-11-05 19:28:02 -0500

    Linear Regression Class

--- FrequencyShiftClassifierRawValues.py
+++ FrequencyShiftClassifierRawValues.py
@@ -5,22 +5,26 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_s
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats import zscore
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
 
 # Read in the csv file that contains all trial data
-dataFile = np.loadtxt('Data/data1N.txt')
+dataFile = np.loadtxt('Data/magRaw.txt')
 ampFile = np.loadtxt('Data/data1Amp.txt')
 
 # Transpose so the labels form a column
 dataFile = dataFile.T
 ampFile = ampFile.T
                        
-X = dataFile[:, :16] # 16 predictor variables
-y = dataFile[:, -1] # Last column is the label
+X = dataFile
 
-#X[:,8:16] = abs(X[:,:8])
+formatted_matrix = np.where(X > 0, 1, np.where(X < 0, -1, 0))
 
-# Normalize with z-score
-#X = (X - X.mean()) / X.std()
+print(formatted_matrix)
+
+
+y = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9]
 
 print(dataFile)
 print(X)
@@ -30,19 +34,17 @@ print(y)
 knn = KNeighborsClassifier(n_neighbors=3)
 
 # Perform cross-validated predictions
-y_pred = cross_val_predict(knn, X, y, cv=4)
+y_pred = cross_val_predict(knn, formatted_matrix, y, cv=3)
 
 # Calculate and display the accuracy score
 accuracy = accuracy_score(y, y_pred)
 print(accuracy)
 print(f"Cross-validated Accuracy: {accuracy:.2f}")
 
-knn.fit(X, y)
-predictions2 = knn.predict(X)
-accuracy2 = accuracy_score(y, predictions2)
-print("Non-cross validated KNN Fit")
-print(accuracy2)
-
+##knn.fit(formatted_matrix, y)
+##predictions2 = knn.predict(formatted_matrix)
+##accuracy2 = accuracy_score(y, predictions2)
+##print(accuracy2)
 
 # Calculate the confusion matrix
 cmlabels = [1,2,3,4,5,6,7,8,9]
@@ -54,4 +56,8 @@ plt.xticks(ticks=np.arange(len(cmlabels)) , labels=cmlabels )
 plt.yticks(ticks=np.arange(len(cmlabels)) , labels=cmlabels)
 plt.show()
 
+
+
+
+                    
 
--- FrequencyShiftRegression.py
+++ FrequencyShiftRegression.py
@@ -8,67 +8,56 @@ from scipy.stats import zscore
 from scipy import stats
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import PolynomialRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
 
 leaveOut = 9
 removeCols = []
 
 # Read in the csv file that contains all trial data
-dataFile = np.loadtxt('Data/data1N.txt')
+dataFile = np.loadtxt('Data/magRaw.txt')
+y = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9]
 
-for col_index, value in enumerate(dataFile[-1]):  # matrix[-1] accesses the last row
+# Transpose so the labels form a column
+dataFile = dataFile.T
+
+X = dataFile
+
+for col_index, value in enumerate(y):  # matrix[-1] accesses the last row
     if value == leaveOut:
         removeCols.append(col_index)
 
 print("Column indices where value in bottom row is 1:", removeCols)
-
-# Transpose so the labels form a column
-dataFile = dataFile.T
                        
-X = dataFile[:, :16] # 16 predictor variables
-y = dataFile[:, -1] # Last column is the label
-
-#print(X[:, 1])
-
 # Filter out the columns of the "leave 1 out" distance
 Xredu = np.delete(X, removeCols, 0);
 yredu = np.delete(y, removeCols);
 Xleftone = X[removeCols, :];
 
 print(Xleftone)
-                      
-# Normalize with z-score
-#X = (X - X.mean()) / X.std()
 
 #print(dataFile)
 print(Xredu)
 print(yredu)
 
-# Initialize the KNN Classifier
-knn = KNeighborsClassifier(n_neighbors=3)
-
-knn.fit(Xredu, yredu)
-
-# Make predictions
-predictions = knn.predict(Xleftone)
-
-print(predictions)
-
-
 # Regression models
 # Simplify to just the frequency shift features now (no sign maps)
-Xredu = Xredu[:,:8]
-X2 = X[:,:8]
-Xleftone = Xleftone[:,:8]
-yredu = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9]
-print(Xredu)
-print(yredu)
+poly = PolynomialFeatures(2)
+
+# Transform the features to polynomial features
+X_poly = poly.fit_transform(X)
 
 model = LinearRegression()
-model.fit(Xredu, yredu)
+model.fit(X, y)
 
-linregpred = model.predict(Xleftone)
+linregpred = model.predict(X_poly)
+print(linregpred)
 mse = mean_squared_error(y, linregpred)
 r2 = r2_score(y, linregpred)
+print(mse)
+print(r2)
 
 
 
