8466b5dfaa53daa39a7f33edf61e20cd194dc267
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2024-11-21 17:57:08 -0500

    Updated Comments + 2nd Half Combined Variables
    Wrote comments for all of the filenames, plus did a combined variable regression run for the 2nd half

--- FrequencyShiftRegression.py
+++ FrequencyShiftRegression.py
@@ -24,25 +24,27 @@ labels2 = 'Data/data2labels.txt'
 frshiftsDecimals = 'Data/data3decimalsfrsh.txt' # 1.5 to 8.5, 2 replication trials each 
 labels3 = 'Data/data3labels.txt'
 frshiftsFirstHalf = 'Data/data4frshfirsthalf.txt' # Integers 1 to 5, 4 replication trials each
-labels4 = 'Data/data4labels.txt' # Labels for first half (1 to 5 cm)
+labelsInt1stHalf = 'Data/data4labels.txt' # Labels for first half (1 to 5 cm)
 frshiftsDecimalsFirstHalf = 'Data/data5decimalsfirsthalf.txt' # 1.5 to 5.5, 2 replication trials each
-labels5 = 'Data/data5labels.txt' # Labels for first half decimals (1.5 to 5.5 cm)
-fftIntsFirstHalf = 'Data/data7FFTintfirsthalf.txt'
-fftDecsFirstHalf = 'Data/data7FFTdecimalfirsthalf.txt'
-fftPowerIntHalf = 'Data/data8FFTintpowerfirsthalf.txt'
-fftPowerDecHalf = 'Data/data8FFTdecpowerfirsthalf.txt'
-frshInts2ndHalf = 'Data/data9frshInt2ndHalf.txt'
-frshDec2ndHalf = 'Data/data9frshDec2ndHalf.txt'
-fftPowerInt2ndHalf = 'Data/data10FFTintpower2ndhalf.txt'
-fftPowerDec2ndHalf = 'Data/data10FFTdecpower2ndhalf.txt'
-labelsInt2ndHalf = 'Data/data9labels.txt'
-labelsDec2ndHalf = 'Data/data10labels.txt'
+labelsDec1stHalf = 'Data/data5labels.txt' # Labels for first half decimals (1.5 to 5.5 cm)
+
+fftIntsFirstHalf = 'Data/data7FFTintfirsthalf.txt' # FFT amplitudes for 1 - 5 cm
+fftDecsFirstHalf = 'Data/data7FFTdecimalfirsthalf.txt' # FFt amplitudes for 1.5 - 5.5 cm
+
+fftPowerInt1stHalf = 'Data/data8FFTintpowerfirsthalf.txt' # FFT dot products for 1 - 5 cm
+fftPowerDec1stHalf = 'Data/data8FFTdecpowerfirsthalf.txt' # FFT dot products for 1.5 - 5.5 cm
+frshInt2ndHalf = 'Data/data9frshInt2ndHalf.txt' # Frequency shifts for 5 - 9 cm
+frshDec2ndHalf = 'Data/data9frshDec2ndHalf.txt' # Frequency shifts for 5.5 - 8.5 cm
+fftPowerInt2ndHalf = 'Data/data10FFTintpower2ndhalf.txt' # FFT dot products for 5 - 9 cm
+fftPowerDec2ndHalf = 'Data/data10FFTdecpower2ndhalf.txt' # FFT dot products for 5.5 - 8.5 cm
+labelsInt2ndHalf = 'Data/data9labels.txt' # Labels for 5 to 9 cm        (1 cm intervals for both of these label variables) 
+labelsDec2ndHalf = 'Data/data10labels.txt' # Labels for 5.5 to 8.5 cm 
 
 trial1 = [frshifts1, frshsigns, pressAmplitudes]
 trial2 = [frshifts2, frshsigns2, pressAmplitudes2]
 
-frshFFTPow2ndHalf = []
-frshFFTPow2ndHalfTest = []
+frshFFTPow2ndHalf = [frshInt2ndHalf, fftPowerInt2ndHalf]
+frshFFTPow2ndHalfTest = [frshDec2ndHalf, fftPowerDec2ndHalf]
 
 # Old control parameters
 performingLeaveOut = False
@@ -57,12 +59,12 @@ testLabelFileName = labelsDec2ndHalf
 
 # Newer control parameters
 numReplications = 4
-polynomialRegressionDegree = 8
-combineVars = False
+polynomialRegressionDegree = 3
+combineVars = True
 normalizeFeature = True
 conductLinearRegression = False
-combineTrainData = trial1
-combineTestData = trial2
+combineTrainData = frshFFTPow2ndHalf#trial1
+combineTestData = frshFFTPow2ndHalfTest #trial2
 
 # Read in the csv file that contains all trial data
 # Assumes that the labels and the features are stored in separate files
@@ -76,6 +78,7 @@ X = []
 if (combineVars): # Combine multiple variable tables into 1 big table 
     for varName in combineTrainData:
         featureTable = np.loadtxt(varName)
+
         if (featureTable.ndim < 2):
             featureTable = np.array([featureTable]).T
         # Normalize each data set separately
@@ -91,15 +94,16 @@ else: # Or just read all variables from 1 table
         X = zscore(X)
 Xtest = []
 if (combineVars): # Combine multiple variable tables into 1 big table 
-    for varName in combineTestData:
+    for varName in combineTestData:        
         featureTable = np.loadtxt(varName)
+
         if (featureTable.ndim < 2):
             featureTable = np.array([featureTable]).T
         # Normalize each data set separately
         if (normalizeFeature):
             featureTable = zscore(featureTable)
         Xtest.append(featureTable)
-    Xtest = np.hstack(X) 
+    Xtest = np.hstack(Xtest) 
 else: # Or just read all variables from 1 table
     if (testFile.ndim < 2):
         testFile = np.array([testFile]).T
